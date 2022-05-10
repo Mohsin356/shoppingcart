9f78590ccdf666153d3313693cf7e7b1c3b65a78
Mohsin.Raza <Mohsin356>
2022-05-10 18:27:00 +0500

    update product 1

--- lib/views/screens/shopping_page.dart
+++ lib/views/screens/shopping_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:shoppingcart/controllers/cart_controller.dart';
 import 'package:shoppingcart/controllers/shopping_controller.dart';
 import 'package:shoppingcart/views/screens/change_name.dart';
